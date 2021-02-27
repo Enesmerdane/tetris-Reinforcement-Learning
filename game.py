e358c64124bee6f5b627806002863d4f04892c46
Enes <ensmrdane@gmail.com>
2021-02-27 16:12:24 +0300

    Score Text
    Score Text

--- game.py
+++ game.py
@@ -1,12 +1,43 @@
 import pygame
 import random
 
+# GAME CONSTANTS
+WINDOW_WIDTH  = 700
+WINDOW_HEIGHT = 800 
+
+BACKGROUND_COLOR = (200, 200, 255)
+
+GAMESCREEN_WIDTH = 400
+GAMESCREEN_HEIGHT = 600
+GAMESCREEN_LOCATION_X = 50
+GAMESCREEN_LOCATION_Y = 50
+
+GAMESCREEN_COLOR = (150, 150, 255)
+
+SCORE_LOCATION_X = 500
+SCORE_LOCATION_Y = 50
+
+#####################################################################################
+def display_score(screen, score):
+    score_text = font.render("Score: " + str(score), True, (0, 0, 0))
+    screen.blit(score_text, [SCORE_LOCATION_X, SCORE_LOCATION_Y])
+
+
+
+
+#####################################################################################
+
 # initialize the pygame
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode( (800, 700) )
+screen = pygame.display.set_mode( (WINDOW_HEIGHT, WINDOW_WIDTH) )
 
+score = 0
+font = pygame.font.SysFont(None, 35)
+
+
+# start game
 running = True
 while running:
     # check for events
@@ -15,11 +46,11 @@ while running:
             running = False
     
     
-    screen.fill( (200, 200, 255) )
+    screen.fill( BACKGROUND_COLOR )
 
-    pygame.draw.rect(screen, (150, 150, 255), [50, 50, 400, 600])
+    pygame.draw.rect(screen, GAMESCREEN_COLOR, [GAMESCREEN_LOCATION_X, GAMESCREEN_LOCATION_Y, GAMESCREEN_WIDTH, GAMESCREEN_HEIGHT])
+
+    display_score(screen, score)
 
     pygame.display.update()
 
-
-    
